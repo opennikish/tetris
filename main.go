fd4d77b09efe74f4e76b09197b292af75c55b3d8
Nikita V <nikita.postpunk@gmail.com>
2025-12-23 23:19:31 +0100

    Cleanup
    - Split game loop with `onTick()` and `oninput()`
    - Rename `PinTetro` to `Tetromino` since we can handle all
    tetrominos with single struct
    - Remove `Tetro` interface
    - Fix context initialization

--- main.go
+++ main.go
@@ -46,6 +46,9 @@ type App struct {
 	exec          func(cmd string, args ...string) error
 	commandReader func(ctx context.Context, stdin io.Reader) (<-chan Command, <-chan error)
 	ticker        AppTicker
+	currTetro     *Tetromino
+	tickCount     int
+	ctxCancel     context.CancelFunc
 }
 
 func NewApp(
@@ -72,7 +75,8 @@ func NewApp(
 
 func (a *App) Start(ctx context.Context) error {
 	log("starting..")
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	a.ctxCancel = stop
 	defer stop()
 
 	if err := a.configureTerminal(); err != nil {
@@ -88,49 +92,15 @@ func (a *App) Start(ctx context.Context) error {
 	clearScreen(a.stdout)
 	a.DrawField()
 	a.render()
-
-	// todo: check if already lended to another tetro and do gameover
-	tetro := a.NextTetro()
-
-	tickCount := 0
+	a.currTetro = a.NextTetro()
 
 	log("start loop")
 	for {
 		select {
 		case cmd := <-cmds:
-			log("cmd: %s", cmd)
-
-			switch cmd {
-			case Quit:
-				a.cursor.SetPos(a.height+4, 0)
-				fmt.Fprintln(a.stdout, "Bye")
-				return nil
-			case Rotate:
-				tetro.Clear(a.cursor, a.stdout)
-				tetro.Rotate()
-				tetro.Draw(a.cursor, a.stdout)
-			case Left:
-				tetro.Clear(a.cursor, a.stdout)
-				tetro.MoveHorizontaly(a.field, -1)
-				tetro.Draw(a.cursor, a.stdout)
-			case Right:
-				tetro.Clear(a.cursor, a.stdout)
-				tetro.MoveHorizontaly(a.field, 1)
-				tetro.Draw(a.cursor, a.stdout)
-			}
-
+			a.onInput(cmd)
 		case <-a.ticker.Channel():
-			log("tick: %d", tickCount)
-			tickCount++
-			if tetro.IsLanded(a.field) {
-				log("is landed")
-				tetro.Cement(a.field)
-				tetro = a.NextTetro()
-			}
-			tetro.Clear(a.cursor, a.stdout)
-			tetro.MoveDown()
-			tetro.Draw(a.cursor, a.stdout)
-
+			a.onTick()
 		case <-ctx.Done():
 			a.cursor.SetPos(a.height+4, 0)
 			fmt.Fprintln(a.stdout, "Bye")
@@ -141,8 +111,48 @@ func (a *App) Start(ctx context.Context) error {
 	}
 }
 
+func (a *App) onTick() {
+	log("tick: %d", a.tickCount)
+	a.tickCount++
+
+	if a.currTetro.IsLanded(a.field) {
+		log("is landed")
+		a.currTetro.Cement(a.field)
+		a.currTetro = a.NextTetro() // todo: check if already lended to another currTetro and do gameover
+	}
+
+	a.currTetro.Clear(a.cursor, a.stdout)
+	a.currTetro.MoveDown()
+	a.currTetro.Draw(a.cursor, a.stdout)
+}
+
+func (a *App) onInput(cmd Command) {
+	log("cmd: %s", cmd)
+
+	switch cmd {
+	case Quit:
+		a.quit()
+	case Rotate:
+		a.currTetro.Clear(a.cursor, a.stdout)
+		a.currTetro.Rotate()
+		a.currTetro.Draw(a.cursor, a.stdout)
+	case Left:
+		a.currTetro.Clear(a.cursor, a.stdout)
+		a.currTetro.MoveHorizontaly(a.field, -1)
+		a.currTetro.Draw(a.cursor, a.stdout)
+	case Right:
+		a.currTetro.Clear(a.cursor, a.stdout)
+		a.currTetro.MoveHorizontaly(a.field, 1)
+		a.currTetro.Draw(a.cursor, a.stdout)
+	}
+}
+
+func (a *App) quit() {
+	a.ctxCancel()
+}
+
 // todo: impl
-func (a *App) NextTetro() Tetro {
+func (a *App) NextTetro() *Tetromino {
 	return NewPinTetro()
 }
 
@@ -241,30 +251,26 @@ func commandReader(ctx context.Context, stdin io.Reader) (<-chan Command, <-chan
 	return cmds, errc
 }
 
-type Tetro interface {
-	MoveHorizontaly(field [][]byte, dir int)
-	MoveDown()
-	Rotate()
-
-	// todo: Remove those and introduce Playfield
-	Clear(*Cursor, io.Writer)
-	Draw(*Cursor, io.Writer)
-	IsLanded([][]byte) bool
-	Cement([][]byte)
-}
-
 type Point struct {
-	x, y    int
-	bracket byte
+	x, y   int
+	symbol byte
 }
 
-type PinTetro struct {
-	rotationPos int
-	Points      [8]Point
+type Dir struct {
+	x, y int
+}
+type Rule struct {
+	Dirs [4]Dir
 }
 
-func NewPinTetro() *PinTetro {
-	return &PinTetro{
+type Tetromino struct {
+	rotationPos   int
+	rotationRules [4]Rule
+	Points        [8]Point
+}
+
+func NewPinTetro() *Tetromino {
+	return &Tetromino{
 		Points: [8]Point{
 			{OffsetLeft + 4*2, OffsetTop, '['},
 			{OffsetLeft + 4*2 + 1, OffsetTop, ']'},
@@ -275,25 +281,17 @@ func NewPinTetro() *PinTetro {
 			{OffsetLeft + 5*2, OffsetTop + 1, '['},
 			{OffsetLeft + 5*2 + 1, OffsetTop + 1, ']'},
 		},
+		rotationRules: [4]Rule{
+			{Dirs: [4]Dir{{1, 1}, {1, -1}, {0, 0}, {-1, 1}}},
+			{Dirs: [4]Dir{{-1, 1}, {1, 1}, {0, 0}, {-1, -1}}},
+			{Dirs: [4]Dir{{-1, -1}, {-1, 1}, {0, 0}, {1, -1}}},
+			{Dirs: [4]Dir{{1, -1}, {-1, -1}, {0, 0}, {1, 1}}},
+		},
 	}
 }
 
-func (t *PinTetro) Rotate() {
-	type Dir struct {
-		x, y int
-	}
-	type Rule struct {
-		Dirs [4]Dir
-	}
-
-	rulesByState := [4]Rule{
-		{Dirs: [4]Dir{{1, 1}, {1, -1}, {0, 0}, {-1, 1}}},
-		{Dirs: [4]Dir{{-1, 1}, {1, 1}, {0, 0}, {-1, -1}}},
-		{Dirs: [4]Dir{{-1, -1}, {-1, 1}, {0, 0}, {1, -1}}},
-		{Dirs: [4]Dir{{1, -1}, {-1, -1}, {0, 0}, {1, 1}}},
-	}
-
-	rule := rulesByState[t.rotationPos]
+func (t *Tetromino) Rotate() {
+	rule := t.rotationRules[t.rotationPos]
 
 	for i := 0; i < len(t.Points); i += 2 {
 		t.Points[i].x += rule.Dirs[i/2].x * 2
@@ -305,32 +303,13 @@ func (t *PinTetro) Rotate() {
 	t.rotationPos = (t.rotationPos + 1) % 4
 }
 
-// todo: Extract Clear and Draw somewhere, it's too much responsibility
-func (t *PinTetro) Clear(cursor *Cursor, stdout io.Writer) {
-	for _, p := range t.Points {
-		empty := ' '
-		if p.bracket == ']' && p.y != OffsetTop {
-			empty = '.'
-		}
-		cursor.SetPos(p.y+1, p.x+1)
-		fmt.Fprintf(stdout, "%c", empty)
-	}
-}
-
-func (t *PinTetro) Draw(cursor *Cursor, stdout io.Writer) {
-	for _, p := range t.Points {
-		cursor.SetPos(p.y+1, p.x+1)
-		fmt.Fprintf(stdout, "%c", p.bracket)
-	}
-}
-
-func (t *PinTetro) MoveDown() {
+func (t *Tetromino) MoveDown() {
 	for i := range 8 {
 		t.Points[i].y += 1
 	}
 }
 
-func (t *PinTetro) MoveHorizontaly(field [][]byte, dir int) {
+func (t *Tetromino) MoveHorizontaly(field [][]byte, dir int) {
 	for _, p := range t.Points {
 		nextPos := p.x + dir*2
 		if nextPos < OffsetLeft || nextPos > OffsetLeft+20 {
@@ -347,13 +326,32 @@ func (t *PinTetro) MoveHorizontaly(field [][]byte, dir int) {
 	}
 }
 
-func (t *PinTetro) Cement(field [][]byte) {
+// todo: Extract Clear and Draw somewhere, it's too much responsibility
+func (t *Tetromino) Clear(cursor *Cursor, stdout io.Writer) {
 	for _, p := range t.Points {
-		field[p.y][p.x] = p.bracket
+		empty := ' '
+		if p.symbol == ']' && p.y != OffsetTop {
+			empty = '.'
+		}
+		cursor.SetPos(p.y+1, p.x+1)
+		fmt.Fprintf(stdout, "%c", empty)
 	}
 }
 
-func (t *PinTetro) IsLanded(field [][]byte) bool {
+func (t *Tetromino) Draw(cursor *Cursor, stdout io.Writer) {
+	for _, p := range t.Points {
+		cursor.SetPos(p.y+1, p.x+1)
+		fmt.Fprintf(stdout, "%c", p.symbol)
+	}
+}
+
+func (t *Tetromino) Cement(field [][]byte) {
+	for _, p := range t.Points {
+		field[p.y][p.x] = p.symbol
+	}
+}
+
+func (t *Tetromino) IsLanded(field [][]byte) bool {
 	for _, p := range t.Points {
 		nextPoint := field[p.y+1][p.x]
 		if p.y == 20 || nextPoint == '[' || nextPoint == ']' {
